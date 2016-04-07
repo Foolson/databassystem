7cc643b6f0e6b5b1f7d9a85002a90df3fa519286
Johan Foolson <d15johol@hs.local>
2016-04-07 13:55:31 +0200

    Svaga entiteter klara

--- databassystem.sql
+++ databassystem.sql
@@ -2,6 +2,7 @@ drop database d15johol;
 create database d15johol;
 use d15johol;
 
+# Starka
 create table domare(
 	pnr char(13),
 	lon char(13),
@@ -33,4 +34,33 @@ create table caddy(
 	favTips varchar(20),
 	namn varchar(20),
 	primary	key(pnr)
+)engine=innodb;
+
+# Svaga
+create table speltillfälle(
+	starttid varchar(20),
+	resultat char(13),
+	namn varchar(20),
+	pnr char(13),
+	primary key(starttid, namn, pnr),
+	foreign key(namn) references tavling(namn),
+	foreign key(pnr) references spelare(pnr)
+)engine=innodb;
+
+create table golfbag(
+	typ varchar(20),
+	marke varchar(20),
+	pnr char(13),
+	primary key(marke, pnr),
+	foreign key(pnr) references spelare(pnr)
+)engine=innodb;
+
+create table klubba(
+	namn varchar(20),
+	kommentar varchar(20),
+	pnr char(13),
+	marke varchar(20),
+	primary key(namn, pnr, marke),
+	foreign key(pnr) references spelare(pnr),
+	foreign key(marke) references golfbag(marke)
 )engine=innodb;
