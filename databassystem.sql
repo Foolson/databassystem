a217c8515ad0098adb7f952b9441464d3e707ea4
Johan Foolson <d15johol@hs.local>
2016-04-07 15:56:37 +0200

    mmbop

--- databassystem.sql
+++ databassystem.sql
@@ -41,7 +41,7 @@ create table boll(
 
 create table speltillfälle(
 	starttid datetime,
-	resultat char(13),
+	resultat varchar(20),
 	namn varchar(20),
 	pnr char(11),
 	primary key(starttid, namn, pnr),
@@ -59,7 +59,7 @@ create table golfbag(
 
 create table klubba(
 	namn varchar(20),
-	kommentar varchar(20),
+	kommentar varchar(30),
 	pnr char(11),
 	marke varchar(20),
 	primary key(namn, pnr, marke),
@@ -80,24 +80,37 @@ create table caddy(
 
 # Transaktioner
 # 1
-insert into tavling values('Sigges sommargolf','2016-10-07');
+insert into tavling values('Sigges sommargolf','2016-07-10');
 
 # 2
 insert into domare values('790129-4444',12000,'Simon');
 insert into domare values('810912-5555',12000,'Sven');
 
-#3
+# 3
 insert into spelare values('560123-6666','Bosse');
 insert into speltillfälle values('2016-10-07 10:25:00',null,'Sigges sommargolf','560123-6666');
 
-#4
+# 4
 insert into spelare values('730909-1111','Reidar');
 insert into golfbag values('Tour','Nike','730909-1111');
 
-#5
+# 5
 insert into klubba values('Driver','Spikrak och kort','730909-1111','Nike');
 
-#6
+# 6
 insert into spelare values('670808-2222','Sune');
 insert into golfbag values('Gogo','Superbrand','670808-2222');
 insert into caddy values('891010-5468','Knyt skorna hårdare','Anna','670808-2222','Superbrand');
+
+# 7
+insert into speltillfälle values('2016-10-07 13:10:00','72','Sigges sommargolf','670808-2222');
+insert into boll values('Hjärta',null,'Titleist','670808-2222');
+
+# 8
+insert into spelare values('940101-8651','Stina');
+insert into golfbag values('Super','Boom','940101-8651');
+insert into klubba values('Driver','Längst och snedast på touren','940101-8651','Boom');
+insert into caddy values('461224-4385','Svinga lugnt','Petra','940101-8651','Boom');
+
+# 9
+insert into domare values('890205-1346',12000,'Göran');
