718d36001dd29c97947f869ba2552af5ce8c5d0a
Foolson <johan.olsson.91@gmail.com>
2016-05-18 08:22:56 +0200

    Stuff

--- databassystem.sql
+++ databassystem.sql
@@ -80,68 +80,78 @@ create table caddy(
 
 # Transaktioner
 # Tävling
-insert into tavling values('Sigges sommargolf','2016-07-10');
-insert into tavling values('Ryder Cup','2014-06-20');
-insert into tavling values('Masters','2077-07-12');
+insert into tavling values
+	('Sigges sommargolf','2016-07-10'),
+	('Ryder Cup','2014-06-20'),
+	('Masters','2077-07-12');
 
 # Domare
-insert into domare values('790129-4444',12000,'Simon');
-insert into domare values('810912-5555',12000,'Sven');
-insert into domare values('790612-1212',12000,'Eva');
-insert into domare values('850906-3597',12000,'Jens');
-insert into domare values('770202-3333',12000,'Göran');
-insert into domare values('780203-7785',55000,'Knugen');
-insert into domare values('910203-2185',4300,'Gullis');
+insert into domare values
+	('790129-4444',12000,'Simon'),
+	('810912-5555',12000,'Sven'),
+	('790612-1212',12000,'Eva'),
+	('850906-3597',12000,'Jens'),
+	('770202-3333',12000,'Göran'),
+	('780203-7785',55000,'Knugen'),
+	('910203-2185',4300,'Gullis');
 
-insert into ansvara values('790129-4444','Sigges sommargolf');
-insert into ansvara values('810912-5555','Sigges sommargolf');
-insert into ansvara values('790612-1212','Sigges sommargolf');
-insert into ansvara values('850906-3597','Ryder Cup');
-insert into ansvara values('850906-3597','Sigges sommargolf');
+# Relation mellan Domare och Tävlingar
+insert into ansvara values
+	('790129-4444','Sigges sommargolf'),
+	('810912-5555','Sigges sommargolf'),
+	('790612-1212','Sigges sommargolf'),
+	('850906-3597','Ryder Cup'),
+	('850906-3597','Sigges sommargolf');
 
 # Spelare
-insert into spelare values('940101-8651','Stina');
-insert into spelare values('670808-2222','Sune');
-insert into spelare values('790101-4343','Benny');
-insert into spelare values('560123-6666','Bosse');
-insert into spelare values('730909-1111','Reidar');
-insert into spelare values('730909-7777','Reidar');
-insert into spelare values('660808-5555','Kurt-Evert');
+insert into spelare values
+	('940101-8651','Stina'),
+	('670808-2222','Sune'),
+	('790101-4343','Benny'),
+	('560123-6666','Bosse'),
+	('730909-1111','Reidar'),
+	('730909-7777','Reidar'),
+	('660808-5555','Kurt-Evert');
 
 # Golfbag
-insert into golfbag values('Super','Boom','940101-8651');
-insert into golfbag values('Gogo','SuperbrAND','670808-2222');
-insert into golfbag values('Tour','Nike','730909-1111');
-insert into golfbag values('Wooow','Titleist','560123-6666');
-insert into golfbag values('AndroidBag','Google','660808-5555');
+insert into golfbag values
+	('Super','Boom','940101-8651'),
+	('Gogo','SuperbrAND','670808-2222'),
+	('Tour','Nike','730909-1111'),
+	('Wooow','Titleist','560123-6666'),
+	('AndroidBag','Google','660808-5555');
 
 # Klubba
-insert into klubba values('Dudle','Säkra bettan','940101-8651','Boom');
-insert into klubba values('Driver','Längst och snedast på touren','940101-8651','Boom');
-insert into klubba values('Driver','Spikrak och kort','730909-1111','Nike');
-insert into klubba values('Jensens','Google suger','730909-1111','Nike');
+insert into klubba values
+	('Dudle','Säkra bettan','940101-8651','Boom'),
+	('Driver','Längst och snedast på touren','940101-8651','Boom'),
+	('Driver','Spikrak och kort','730909-1111','Nike'),
+	('Jensens','Google suger','730909-1111','Nike');
 
 # Caddy
-insert into caddy values('891010-5468','Knyt skorna hårdare','Anna','670808-2222','SuperbrAND');
-insert into caddy values('461224-4385','Svinga lugnt','Petra','940101-8651','Boom');
-insert into caddy values('551221-9988','Sluta stirra på brudarnas rövar','Jesus','660808-5555','Google');
-insert into caddy values('991221-1235','Sluta stirra','Jeppe','560123-6666','Titleist');	
+insert into caddy values
+	('891010-5468','Knyt skorna hårdare','Anna','670808-2222','SuperbrAND'),
+	('461224-4385','Svinga lugnt','Petra','940101-8651','Boom'),
+	('551221-9988','Sluta stirra på brudarnas rövar','Jesus','660808-5555','Google'),
+	('991221-1235','Sluta stirra','Jeppe','560123-6666','Titleist');	
 
 # Boll
-insert into boll values('Hjärta',null,'Titleist','670808-2222');
-insert into boll values('Tre prickar',null,'Titleist','790101-4343');
-insert into boll values('Superball',null,'Nike','940101-8651');
-insert into boll values('RANDig',null,'Kina','560123-6666');
+insert into boll values
+	('Hjärta',null,'Titleist','670808-2222'),
+	('Tre prickar',null,'Titleist','790101-4343'),
+	('Superball',null,'Nike','940101-8651'),
+	('RANDig',null,'Kina','560123-6666');
 
 # Speltillfälle
-insert into speltillfälle values('2016-10-07 13:10:00',72,'Sigges sommargolf','670808-2222');
-insert into speltillfälle values('2016-10-07 10:25:00',0,'Sigges sommargolf','560123-6666');
-insert into speltillfälle values('2016-10-07 14:10:00',0,'Sigges sommargolf','790101-4343');
-insert into speltillfälle values('2016-10-07 12:05:00',0,'Sigges sommargolf','940101-8651');
-insert into speltillfälle values('2014-06-20 12:00:00',72,'Ryder Cup','660808-5555');
-insert into speltillfälle values('2077-07-12 11:56:00',89,'Masters','940101-8651');
-insert into speltillfälle values('2077-07-12 13:12:00',10,'Masters','790101-4343');
-insert into speltillfälle values('2077-07-12 13:55:00',51,'Masters','730909-7777');
+insert into speltillfälle values
+	('2016-10-07 13:10:00',72,'Sigges sommargolf','670808-2222'),
+	('2016-10-07 10:25:00',0,'Sigges sommargolf','560123-6666'),
+	('2016-10-07 14:10:00',0,'Sigges sommargolf','790101-4343'),
+	('2016-10-07 12:05:00',0,'Sigges sommargolf','940101-8651'),
+	('2014-06-20 12:00:00',72,'Ryder Cup','660808-5555'),
+	('2077-07-12 11:56:00',89,'Masters','940101-8651'),
+	('2077-07-12 13:12:00',10,'Masters','790101-4343'),
+	('2077-07-12 13:55:00',51,'Masters','730909-7777');
 
 # Frågeställningar
 SELECT namn 
@@ -216,7 +226,7 @@ SELECT *
 	FROM speltillfälle
     WHERE speltillfälle.starttid BETWEEN '2077-07-10' AND '2077-07-17';
 
-SET SQL_SAFE_UP	DATES=0;
+SET SQL_SAFE_UPDATES=0;
 
 UPDATE domare
     SET lon=lon*1.03
