32217ba9a4a3b61537a9dc632a12368b9a7f770f
Johan Olsson <johan.olsson.91@gmail.com>
2016-05-20 08:51:28 +0200

    Börjat kommentera

--- databassystem.sql
+++ databassystem.sql
@@ -3,6 +3,7 @@ create database d15johol;
 use d15johol;
 
 # Skapa tabeller
+# Skapa tabellen domare
 create table domare(
 	pnr char(11),
 	lon int,
@@ -10,12 +11,14 @@ create table domare(
 	primary key(pnr)
 )engine=innodb;
 
+# Skapa tabellen tavling
 create table tavling(
 	namn varchar(20),
 	datum date,
 	primary key(namn)
 )engine=innodb;
-	
+
+# Skapa tabellen ansvara för relationen mellan tavling och domare
 create table ansvara(
 	pnr char(11),
 	namn varchar(20),
@@ -24,22 +27,25 @@ create table ansvara(
 	foreign key(pnr) references domare(pnr)
 )engine=innodb;
 
+# Skapa tabellen spelare
 create table spelare(
 	pnr char(11),
 	namn varchar(20),
 	primary key(pnr)
 )engine=innodb;
 
+# Skapa tabellen boll
 create table boll(
 	signatur varchar(20),
-	nr char(11),
+	nr int,
 	marke varchar(20),
 	pnr char(11),
 	primary key(signatur),
 	foreign key(pnr) references spelare(pnr)
 )engine=innodb;
 
-create table speltillfälle(
+# Skapa tabellen speltillfalle
+create table speltillfalle(
 	starttid datetime,
 	resultat int,
 	namn varchar(20),
@@ -49,6 +55,7 @@ create table speltillfälle(
 	foreign key(pnr) references spelare(pnr)
 )engine=innodb;
 
+# Skapa tabellen golfbag
 create table golfbag(
 	typ varchar(20),
 	marke varchar(20),
@@ -57,6 +64,7 @@ create table golfbag(
 	foreign key(pnr) references spelare(pnr)
 )engine=innodb;
 
+# Skapa tabellen klubba
 create table klubba(
 	namn varchar(20),
 	kommentar varchar(30),
@@ -67,6 +75,7 @@ create table klubba(
 	foreign key(marke) references golfbag(marke)
 )engine=innodb;
 
+# Skapa tabellen caddy
 create table caddy(
 	pnr char(11),
 	favTips varchar(50),
@@ -79,7 +88,7 @@ create table caddy(
 )engine=innodb;
 
 # Transaktioner
-# Tävling
+# Lägg in data i tabellen ävling
 insert into tavling values
 	('Sigges sommargolf','2016-07-10'),
 	('Ryder Cup','2014-06-20'),
@@ -133,17 +142,17 @@ insert into caddy values
 	('891010-5468','Knyt skorna hårdare','Anna','670808-2222','SuperbrAND'),
 	('461224-4385','Svinga lugnt','Petra','940101-8651','Boom'),
 	('551221-9988','Sluta stirra på brudarnas rövar','Jesus','660808-5555','Google'),
-	('991221-1235','Sluta stirra','Jeppe','560123-6666','Titleist');	
+	('991221-1235','Sluta stirra','Jeppe','560123-6666','Titleist');
 
 # Boll
 insert into boll values
-	('Hjärta',null,'Titleist','670808-2222'),
-	('Tre prickar',null,'Titleist','790101-4343'),
-	('Superball',null,'Nike','940101-8651'),
-	('RANDig',null,'Kina','560123-6666');
+	('Hjärta',0,'Titleist','670808-2222'),
+	('Tre prickar',0,'Titleist','790101-4343'),
+	('Superball',0,'Nike','940101-8651'),
+	('RANDig',0,'Kina','560123-6666');
 
-# Speltillfälle
-insert into speltillfälle values
+# Speltillfalle
+insert into speltillfalle values
 	('2016-10-07 13:10:00',72,'Sigges sommargolf','670808-2222'),
 	('2016-10-07 10:25:00',0,'Sigges sommargolf','560123-6666'),
 	('2016-10-07 14:10:00',0,'Sigges sommargolf','790101-4343'),
@@ -154,86 +163,90 @@ insert into speltillfälle values
 	('2077-07-12 13:55:00',51,'Masters','730909-7777');
 
 # Frågeställningar
-SELECT namn 
-	FROM domare 
+SELECT namn
+	FROM domare
 	WHERE pnr='790129-4444';
 
-SELECT signatur 
-	FROM boll 
+SELECT signatur
+	FROM boll
 	WHERE pnr='560123-6666';
 
-SELECT typ 
-	FROM golfbag 
-    WHERE pnr='560123-6666';
+SELECT typ
+	FROM golfbag
+  WHERE pnr='560123-6666';
 
-SELECT spelare.namn 
-	FROM spelare, boll 
-    WHERE boll.pnr=spelare.pnr AND boll.marke='Titleist';
+SELECT spelare.namn
+	FROM spelare, boll
+  WHERE boll.pnr=spelare.pnr AND boll.marke='Titleist';
 
-SELECT speltillfälle.resultat
-	FROM speltillfälle, spelare, boll 
-    WHERE speltillfälle.pnr=spelare.pnr AND spelare.pnr=boll.pnr AND boll.marke='Nike';
+SELECT speltillfalle.resultat
+	FROM speltillfalle, spelare, boll
+  WHERE speltillfalle.pnr=spelare.pnr AND spelare.pnr=boll.pnr AND boll.marke='Nike';
 
 SELECT spelare.pnr
-	FROM spelare, spelare as bspelare 
-    WHERE spelare.namn=bspelare.namn AND spelare.pnr!=bspelare.pnr;
+	FROM spelare, spelare as bspelare
+  WHERE spelare.namn=bspelare.namn AND spelare.pnr!=bspelare.pnr;
 
 SELECT domare.pnr
 	FROM domare
-    WHERE NOT EXISTS(SELECT ansvara.pnr FROM ansvara WHERE domare.pnr=ansvara.pnr);
-    
+  WHERE NOT EXISTS(
+							SELECT ansvara.pnr
+	            	FROM ansvara
+								WHERE domare.pnr=ansvara.pnr
+            );
+
 SELECT caddy.favTips
-	FROM caddy, speltillfälle
-    WHERE caddy.spelPnr='660808-5555' AND speltillfälle.pnr='660808-5555' AND speltillfälle.resultat='72' AND speltillfälle.namn='Ryder Cup';
-    
+	FROM caddy, speltillfalle
+  WHERE caddy.spelPnr='660808-5555' AND speltillfalle.pnr='660808-5555' AND speltillfalle.resultat='72' AND speltillfalle.namn='Ryder Cup';
+
 SELECT spelare.*
-	FROM spelare, speltillfälle
-    WHERE spelare.pnr=speltillfälle.pnr;
+	FROM spelare, speltillfalle
+  WHERE spelare.pnr=speltillfalle.pnr;
 
 SELECT domare.*
 	FROM domare, ansvara
-    WHERE domare.pnr=ansvara.pnr
-    GROUP BY domare.pnr
-    HAVING COUNT(*)=2;
+  WHERE domare.pnr=ansvara.pnr
+  GROUP BY domare.pnr
+  HAVING COUNT(*)=2;
 
 SELECT *
 	FROM golfbag
-    ORDER BY golfbag.marke DESC;
-	
+  ORDER BY golfbag.marke DESC;
+
 SELECT AVG(resultat) AS Medelresultat
-	FROM speltillfälle;
-    
-SELECT tavling.namn AS Tävling, AVG(speltillfälle.resultat) AS Medelresultat
-	FROM tavling, speltillfälle
-    WHERE tavling.namn=speltillfälle.namn
-    GROUP BY tavling.namn;
-    
+	FROM speltillfalle;
+
+SELECT tavling.namn AS Tävling, AVG(speltillfalle.resultat) AS Medelresultat
+	FROM tavling, speltillfalle
+  WHERE tavling.namn=speltillfalle.namn
+  GROUP BY tavling.namn;
+
 SELECT *
 	FROM klubba
-    WHERE klubba.namn LIKE 'J%';
+  WHERE klubba.namn LIKE 'J%';
 
 SELECT spelare.namn
-	FROM spelare, speltillfälle
-    WHERE speltillfälle.namn='Masters' AND spelare.pnr=speltillfälle.pnr
-    ORDER BY speltillfälle.resultat ASC
-    LIMIT 1;
-    
+	FROM spelare, speltillfalle
+  WHERE speltillfalle.namn='Masters' AND spelare.pnr=speltillfalle.pnr
+  ORDER BY speltillfalle.resultat ASC
+  LIMIT 1;
+
 SELECT spelare.namn
-	FROM spelare, speltillfälle
-    WHERE speltillfälle.resultat=0 AND spelare.pnr=speltillfälle.pnr;
+	FROM spelare, speltillfalle
+  WHERE speltillfalle.resultat=0 AND spelare.pnr=speltillfalle.pnr;
 
 SELECT *
-	FROM speltillfälle
-    WHERE speltillfälle.starttid BETWEEN '2077-07-10' AND '2077-07-17';
+	FROM speltillfalle
+  WHERE speltillfalle.starttid BETWEEN '2077-07-10' AND '2077-07-17';
 
 SET SQL_SAFE_UPDATES=0;
 
 UPDATE domare
-    SET lon=lon*1.03
-    WHERE lon BETWEEN 10000 AND 12000;
+  SET lon=lon*1.03
+  WHERE lon BETWEEN 10000 AND 12000;
 
 DELETE FROM caddy
-    WHERE favTips='Sluta stirra';
+  WHERE favTips='Sluta stirra';
 
 DELETE FROM golfbag
 	WHERE marke='Titleist' AND pnr='560123-6666';
